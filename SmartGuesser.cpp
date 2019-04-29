490627848f80b2700da933fd52034888688081dc
8hemlet8 <barel987@gmail.com>
2019-04-29 20:29:49 +0300

    10

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -2,27 +2,49 @@
 #include <stdlib.h>
 using namespace std;
 
-string lastGuess="";
-string nextGuess="";
+string lastGuess = "";
+string nextGuess = "";
+int amnt;
+vector<string> vec;
+string SmartGuesser::guess()
+{
 
-string SmartGuesser::guess() {
-	
-	if(nextGuess.compare("")==0){
-		lastGuess="1234";
+	if (nextGuess.compare("") == 0)
+	{
+		lastGuess = "1234";
 		return "1234";
 	}
-	else {
-		lastGuess=nextGuess;
+	else
+	{
+		lastGuess = nextGuess;
 		return nextGuess;
 	}
 }
-void SmartGuesser::learn(string s){
+void SmartGuesser::learn(string s)
+{
+}
 
+void optionArr(string curr,int size)
+{
+	if(size==1){
+		for(int i=0;i<10;i++){
+			vec.push_back(curr+to_string(i));
+			cout << "Added: " << curr << to_string(i)+" ";
+		}
+		return;
+	}
+	else{
+		for(int i=0;i<10;i++){
+			optionArr(curr+to_string(i),size-1);
+		}
+	}
 
 
 }
-void SmartGuesser::startNewGame(uint ut) {
-	length=ut;
+void SmartGuesser::startNewGame(uint ut)
+{
+	cout << "NEW GAME";
+	length = ut;
+	amnt = pow(9, length);
+	optionArr("",length);
 }
-
-
